38ee0130d996e0a2d42019cfd87980220f3cfcca
Alex Dmitriev <107206552+Carbofoss@users.noreply.github.com>
2023-12-29 00:11:00 +0300

    Add files via upload

---
+++ MyFirstProject/MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,19 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by MacBook on 30.08.2022.
+//
+
+import SwiftUI
+
+
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
